c67f7cfc5e1b2988b04270908d47220c4ae9e7a9
Kevin <kevin.c.souza@gmail.com>
2025-01-23 17:07:25 -0300

    resolve packages, fix dotenv, fix translate datetime, implements swagger

--- api/base/base_model.py
+++ api/base/base_model.py
@@ -6,7 +6,7 @@ class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid6.uuid7, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
     
     class Meta:
         abstract = True
