4fecbedb322e00a3842fb6d7d41a320fadeb156d
Guilherme Valeriano <guilherme.hvaleriano@gmail.com>
2025-01-30 11:36:49 -0300

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'api.middleware.AuthenticationMiddleware'
+    'api.middleware.AuthenticationMiddleware'
 ]
 
 ROOT_URLCONF = 'api.urls'
