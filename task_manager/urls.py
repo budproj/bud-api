0e9bd18a580a90a6afa3a1fef8779f3a613ed57b
Guilherme Valeriano <guilherme.hvaleriano@gmail.com>
2025-03-21 12:42:29 -0300

    pass trailing slash in router constructor to not redirect 301 http status

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
 from task_manager.viewset import TaskViewset
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=True)
 router.register(r'task', TaskViewset, basename="task")
 
 task_list = TaskViewset.as_view({
