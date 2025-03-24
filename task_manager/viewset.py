083db7b8ee7ed278ddeb47497c3ea838b1de83c7
Guilherme Valeriano <guilherme.hvaleriano@gmail.com>
2025-03-24 11:35:17 -0300

    change update method

--- task_manager/viewset.py
+++ task_manager/viewset.py
@@ -85,12 +85,15 @@ class TaskViewset(viewsets.ViewSet):
     def update(self, request, task_id=None, team_id=None):
         try:
             task = get_object_or_404(Task, id=task_id)
+            if not task:
+                return Response({"error": "Task not found"}, status=404)
         except Task.DoesNotExist:
             return Response({"error": "Task not found"}, status=404)
-        serializer = TaskSerializer(task, data=request.data, partial=True)
+        data = request.data.copy()
+        serializer = TaskSerializer(data=data, partial=True)
         
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=400)
       
