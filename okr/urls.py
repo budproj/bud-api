668d16d85811576e68bd343cba63c38d72c50c9b
Guilherme Valeriano <guilherme.hvaleriano@gmail.com>
2025-04-25 16:03:55 -0300

    add: new endpoint to get kr infos and get krs where the owner is in support team

--- okr/urls.py
+++ okr/urls.py
@@ -4,5 +4,6 @@ from okr.viewset import KeyResultViewset, CycleViewset
 urlpatterns = [
     path('kr/<team_id>/<objective_id>/', KeyResultViewset.as_view({'get': 'list'})),
     path('kr/owner/<owner>/<objective_id>/', KeyResultViewset.as_view({'get': 'list_by_user'})),
+    path('kr/<kr_id>/', KeyResultViewset.as_view({'get': 'list_one'})),
     path('cycle/<team_id>/', CycleViewset.as_view({'get': 'list'})),
 ]
--- okr/viewset.py
+++ okr/viewset.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from django.db.models import Q
 
 from okr.serializers.key_result_serializer import KeyResultSerializer
 from okr.serializers.cycle_serializer import CycleSerializer
@@ -17,12 +19,27 @@ class KeyResultViewset(ModelViewSet):
         return Response(serializer.data)
     
     def list_by_user(self, request, owner=None, objective_id=None):
-        queryset = KeyResult.objects.filter(deleted_at=None, owner=owner, objective__cycle__active=True, team__isnull=False)
+        queryset = KeyResult.objects.filter(
+            deleted_at=None,
+            objective__cycle__active=True,
+            team__isnull=False
+        ).filter(
+            Q(owner=owner) | Q(support_team__in=[owner])
+        )
 
         if objective_id not in [None, '0']:
             queryset = queryset.filter(objective__id=objective_id)
 
-        serializer = KeyResultSerializer(queryset, many=True)
+        serializer = KeyResultSerializer(queryset.distinct(), many=True)
+        return Response(serializer.data)
+    
+    def list_one(self, request, kr_id=None):
+        try:
+            key_result = KeyResult.objects.get(id=kr_id, deleted_at=None)
+        except KeyResult.DoesNotExist:
+            raise NotFound(detail="KeyResult not found.")
+
+        serializer = KeyResultSerializer(key_result)
         return Response(serializer.data)
 
 class CycleViewset(ModelViewSet):
