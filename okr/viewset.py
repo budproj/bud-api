4797583562979fa8626a2f366be968c26562a9bf
Kevin <kevin.c.souza@gmail.com>
2025-05-22 14:51:55 -0300

    fix Key Result filters

--- okr/viewset.py
+++ okr/viewset.py
@@ -1,8 +1,10 @@
 from django.db.models import Q
+
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
+from okr.enums.key_result_choices import KeyResultModeChoices
 from okr.serializers.key_result_serializer import KeyResultSerializer
 from okr.serializers.cycle_serializer import CycleSerializer
 from okr.models import KeyResult, Cycle
@@ -29,7 +31,11 @@ class KeyResultViewset(ModelViewSet):
         return Response(serializer.data)
     
     def list_by_user_with_tasks(self, request, pk):
-        queryset = KeyResult.objects.all()
+        queryset = KeyResult.objects.filter(
+            mode=KeyResultModeChoices.PUBLISHED,
+            deleted_at__isnull=True,
+            objective__cycle__active=True,
+        )
         queryset = queryset.filter(
             Q(owner__id=pk) | 
             Q(support_team__in=[pk])
